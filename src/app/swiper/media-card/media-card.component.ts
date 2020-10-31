314328276dd22a30dcaf51368db6314dc2ebdf22
felhe <10731299+felhe@users.noreply.github.com>
2020-10-31 15:40:11 +0100

    feat: add loading skeleton for cover images

--- src/app/swiper/media-card/media-card.component.ts
+++ src/app/swiper/media-card/media-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from "@angular/core";
 import { Medium } from '../classes/Medium';
 
 @Component({
   selector: 'app-media-card',
   templateUrl: './media-card.component.html',
-  styleUrls: ['./media-card.component.css']
+  styleUrls: ['./media-card.component.scss']
 })
-export class MediaCardComponent implements OnInit {
+export class MediaCardComponent implements OnInit, OnChanges {
   @Input() medium: Medium;
+
+  imageLoading: boolean;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.medium && changes.medium.currentValue) {
+      this.imageLoading = true;
+    }
+  }
 }
