cdac3ec603856a30d7ecea0afa6de6c1bd0f449e
felhe <10731299+felhe@users.noreply.github.com>
2020-10-31 14:56:30 +0100

    feat: mock media service

---
+++ src/app/swiper/classes/Medium.ts
@@ -0,0 +1,19 @@
+export class Medium {
+  type: 'tvshows' | 'movies';
+  genres: string[];
+  genre_ids: number[];
+  image: string;
+  link: string;
+  region: 'de' | string;
+  title: string;
+  id: number;
+  popularity: number;
+  vote_count: number;
+  vote_average: number;
+  poster_path: string;
+  original_language: string;
+  original_title?: string;
+  backdrop_path: string;
+  overview: string;
+  adult?: boolean;
+}
